3d47c0cab67f9a571241a859f9e684b9fc9ab3a2
fatihi-ad <74970085+fatihi-ad@users.noreply.github.com>
2021-04-09 21:00:42 +0200

    Add files via upload
    depot php

---
+++ exercice php-1.php
@@ -0,0 +1,11 @@
+                        <!-- *********L'instruction echo********** -->
+<html>
+<body>
+  <?php 
+      echo"Bonjour le monde de developpeurs,es ce que le metier vous plait?\n";
+      
+      $a = "Winter"; 
+echo $a . " is coming !";
+  ?> 
+</body>
+</html>
---
+++ exercice.php
@@ -0,0 +1,35 @@
+                              <!-- *******Exercice début php base******* -->
+
+<html>
+<body>
+  <?php 
+      echo "J'obtiendrai mon titre quelqu'en soient les difficultés"; 
+  ?> 
+</body>
+</html>
+
+<!-- Les types
+En PHP, le type de la variable est déterminé au moment de son initialisation, pas de sa déclaration.
+
+$a = 12 ;       // un entier
+$b = "Bonjour"; // une chaîne de caractères -->
+
+
+<!-- PHP supporte les types suivants :
+
+le type int (ou integer) pour les entiers
+le type float, double ou real pour les nombres décimaux
+le type string pour les chaînes de caractères ;
+le type boolean, pour les valeurs booléennes : vrai (true) ou faux (false)
+le type Array pour les tableaux
+le type Object pour les objets -->
+
+<!-- Les opérateurs
+Opérateurs arithmétiques
+$a + $b : addition
+$a - $b : soustraction
+$a * $b : multiplication
+$a / $b : division
+$a % $b : modulo (reste de la division de a par b)
+$a++ : incrément (Equivalent à $a = a$ +1)
+$a-- : décrément (Equivalent à $a = a$ -1) -->
